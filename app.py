2d66845e1b6e19b3946abda1b2db9e4992407d57
Faheem-ml-72 <faheemmuhd200@gmail.com>
2026-02-15 11:53:18 +0530

    Initial commit - Tesla Gaussian Process API

---
+++ app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import datetime
+
+# Load trained model
+model = joblib.load("model/tesla_gp_model.pkl")
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Tesla Stock Prediction API is Running"}
+
+@app.get("/predict")
+def predict(days_ahead: int):
+
+    # Get today's date
+    today = datetime.date.today()
+    future_date = today + datetime.timedelta(days=days_ahead)
+
+    # Convert date to ordinal
+    future_ordinal = np.array([[future_date.toordinal()]])
+
+    # Predict
+    prediction, sigma = model.predict(future_ordinal, return_std=True)
+
+    return {
+        "predicted_price": float(prediction[0]),
+        "lower_bound": float(prediction[0] - 2*sigma[0]),
+        "upper_bound": float(prediction[0] + 2*sigma[0])
+    }
