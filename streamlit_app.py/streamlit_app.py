2168c2f0d1c6367d233bc349bf4c7f0f114ee280
Faheem-ml-72 <faheemmuhd200@gmail.com>
2026-02-16 13:45:04 +0530

    Updated API to use LSTM model and scaler

--- streamlit_app.py/streamlit_app.py
+++ streamlit_app.py/streamlit_app.py
@@ -6,10 +6,15 @@ st.title("Tesla Stock Prediction")
 days = st.number_input("Enter number of days ahead:", min_value=1, step=1)
 
 if st.button("Predict"):
-    response = requests.get(f"https://tesla-stock-api-faheem.onrender.com/predict?days_ahead={days}")
-    
+
+    url = f"https://tesla-stock-api-faheem.onrender.com/predict/{days}"
+    response = requests.get(url)
+
     if response.status_code == 200:
         result = response.json()
         st.success(f"Predicted Price: {result['predicted_price']}")
+        st.write(f"Lower Bound: {result['lower_bound']}")
+        st.write(f"Upper Bound: {result['upper_bound']}")
     else:
         st.error("Error fetching prediction")
+
